5d835b2bec4a97cee4f7d5dd802bf266731c3c89
Lujayn Megally <lujaynmegally1@gmail.com>
2026-01-22 01:11:11 +0000

    done

---
+++ models/walmart_dim_fact/walmart_fact_table.sql
@@ -0,0 +1,27 @@
+{{ config(
+    materialized='table',
+    database='WALMART',
+    schema='BRONZE',
+    alias='walmart_fact_table'
+) }}
+
+SELECT
+    store_id,
+    dept_id,
+    store_date,
+    store_weekly_sales,
+    fuel_price,
+    store_temperature,
+    unemployment,
+    cpi,
+    markdown1,
+    markdown2,
+    markdown3,
+    markdown4,
+    markdown5,
+    insert_date,
+    update_date, 
+    dbt_valid_from AS vrsn_start_date,
+    dbt_valid_to AS vrsn_end_date
+FROM WALMART.SNAPSHOTS.walmart_fact_table_snapshot
+WHERE dbt_valid_to IS NULL
